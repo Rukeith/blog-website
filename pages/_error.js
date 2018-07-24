4e43f0e69aa68d00338355fc6e06ec76fb879d09
Edwin Jarvis <fenglan.crazydance@gmail.com>
2018-07-24 21:45:12 +0800

    Add error page

---
+++ pages/_error.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../static/error.scss';
+
+export default class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : 404;
+    return { statusCode, message: JSON.stringify(err) };
+  }
+
+  render() {
+    const { statusCode, message } = this.props;
+    return (
+      <main id="not-found-page">
+        <div className="not-found-page-text-block">
+          <h1>
+            Oops... The page is not found.
+          </h1>
+          <h3>
+            {'You found kitty\'s play room.'}
+          </h3>
+          <h3>
+            {
+              `Play with them ? You got ${statusCode}, error on ${message}`
+            }
+          </h3>
+        </div>
+        <img src="https://firebasestorage.googleapis.com/v0/b/blog-5d8c6.appspot.com/o/system%2Fbackground.jpg?alt=media&token=b8671014-35f4-46b9-903c-20f96c80fef8" alt="cat-404-background" />
+      </main>
+    );
+  }
+}
+
+Error.propTypes = {
+  statusCode: PropTypes.number.isRequired,
+  message: PropTypes.string.isRequired,
+};
