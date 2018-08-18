106d1399adc4020e684a168d3484d6eee9bdc419
Edwin Jarvis <fenglan.crazydance@gmail.com>
2018-08-18 15:49:26 +0800

    1. Update login page style 2. Add vscode launch json 3. Add Article Card(not done)

--- components/BlogPage/ArticleArea/index.js
+++ components/BlogPage/ArticleArea/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import ArticleCard from '../../ArticleCard';
 import './style.scss';
+
+const contentList = [
+  {
+    url: '/intro',
+    content: 'This is a demo content',
+    title: 'demo',
+    coverImage: 'https://react-etc.net/files/2018-01/next-nuxt.png',
+  },
+  {
+    url: '/intro',
+    content: 'This is a demo content 2',
+    title: 'demo',
+    coverImage: '',
+  },
+];
 
 export default () => (
   <section id="article-area">
-    Article Area
+    {
+      contentList.map(item => (
+        <ArticleCard
+          key={item.content}
+          title={item.title}
+          url={item.url}
+          content={item.content}
+          coverImage={item.coverImage}
+        />
+      ))
+    }
   </section>
 );
